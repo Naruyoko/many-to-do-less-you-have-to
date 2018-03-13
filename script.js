182d7c342a5ea71cf455d11c61aae84eb50b6020
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-12 21:11:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-var game{};
+var game{
+  currency:{
+    existance:0,
+    time:0,
+    thought:0,
+    energy:0
+  },
+  production:{
+    existance:0.01,
+    time:0,
+    thought:0,
+    energy:0
+  }
+};
+var timeelapsed=0;
+var lasttime=0;
+function passive(){
+  if (lasttime!=0){timeelapsed=(Date.getTime()-lasttime)/1000;}
+  lasttime=Date.getTime();
+  game.currency.existance+=game.production.existance*timeelapsed;
+  game.currency.time+=game.production.time*timeelapsed;
+  game.currency.thought+=game.production.thought*timeelapsed;
+  game.currency.energy+=game.production.energy*timeelapsed;
+}
