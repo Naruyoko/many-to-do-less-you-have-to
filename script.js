96235caf5ed2af09bf5d503c223baa0009f0f0bd
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-13 20:22:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var game={
   currency:{
-    existance:0,
+    existivity:0,
     time:0,
     thought:0,
     energy:0
   },
   production:{
-    existance:0.01,
+    existivity:0.01,
     time:0,
     thought:0,
     energy:0
   },
   unlocked:{
-    existance:true,
+    existivity:true,
     time:false,
     thought:false,
     energy:false
@@ -26,11 +26,12 @@ function passive(){
     d=new Date();
     timeelapsed=(d.getTime()-lasttime)/1000;}
   lasttime=d.getTime();
-  game.currency.existance+=game.production.existance*timeelapsed;
+  game.currency.existivity+=game.production.existivity*timeelapsed;
+  game.currency.existance=1/(1+Math.exp(game.currency.existance));
   game.currency.time+=game.production.time*timeelapsed;
   game.currency.thought+=game.production.thought*timeelapsed;
   game.currency.energy+=game.production.energy*timeelapsed;
-  if (game.unlocked.existance){document.getElementById("disp.existance").innerHTML="Existance: <span class=\"large\">"+game.currency.existance+"</span>";}
+  if (game.unlocked.existivity){document.getElementById("disp.existivity").innerHTML="Your existivity is <span class=\"large\">"+Math.round(game.currency.existivity*10)/10+"</span> and has <span class=\"large\">"+Math.round(game.currency.existance*1000)/10+"%</span> change of existing.";}
   if (game.unlocked.time){document.getElementById("disp.time").innerHTML="Time: <span class=\"large\">"+game.currency.time+"</span>";}
   if (game.unlocked.thought){document.getElementById("disp.thought").innerHTML="Thoughts: <span class=\"large\">"+game.currency.thought+"</span>";}
   if (game.unlocked.energy){document.getElementById("disp.energy").innerHTML="Energy: <span class=\"large\">"+game.currency.energy+"</span>";}
