87094e982b7103561f20c5104c384b3d27512c6b
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-13 20:44:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ function passive(){
     timeelapsed=(d.getTime()-lasttime)/1000;}
   lasttime=d.getTime();
   game.currency.existivity+=game.production.existivity*timeelapsed;
-  game.currency.existance=1/(1+Math.exp(game.currency.existivity));
+  game.currency.existance=2/(1+Math.exp(game.currency.existivity))-1;
   game.currency.time+=game.production.time*timeelapsed;
   game.currency.thought+=game.production.thought*timeelapsed;
   game.currency.energy+=game.production.energy*timeelapsed;
-  if (game.unlocked.existivity){document.getElementById("disp.existivity").innerHTML="Your existivity is <span class=\"large\">"+Math.round(game.currency.existivity*10)/10+"</span> and has <span class=\"large\">"+Math.round(1000*game.currency.existivity)/10+"%</span> change of existing.";}
+  if (game.unlocked.existivity){document.getElementById("disp.existivity").innerHTML="Your existivity is <span class=\"large\">"+Math.round(game.currency.existivity*10)/10+"</span> and has <span class=\"large\">"+Math.round(1000*game.currency.existance)/10+"%</span> change of existing.";}
   if (game.unlocked.time){document.getElementById("disp.time").innerHTML="Time: <span class=\"large\">"+game.currency.time+"</span>";}
   if (game.unlocked.thought){document.getElementById("disp.thought").innerHTML="Thoughts: <span class=\"large\">"+game.currency.thought+"</span>";}
   if (game.unlocked.energy){document.getElementById("disp.energy").innerHTML="Energy: <span class=\"large\">"+game.currency.energy+"</span>";}
