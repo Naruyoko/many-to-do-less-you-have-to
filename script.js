60c411e4bd744bf4ec2f8b18e7e9f0dfd3a09122
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-14 07:27:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ var game={
     existivity:0,
     existability:0,
     existance:0,
-    time:0,
+    etime:0, //experienced time
     thought:0,
     energy:0
   },
   production:{
     existivity:0.01,
     existance:0,
-    time:0,
+    etime:0,
     thought:0,
     energy:0
   },
   unlocked:{
     existivity:true,
     existance:false,
-    time:false,
+    etime:false,
     thought:false,
     energy:false
   }
 };
+function showhide(x,t){
+  if (t){
+      x.style.display="block";
+  }else{
+      x.style.display="none";
+  }
+}
 var timeelapsed=0;
 var lasttime=0;
 var d=new Date();
@@ -32,12 +39,12 @@ function passive(){
   lasttime=d.getTime();
   game.currency.existivity+=game.production.existivity*timeelapsed;
   game.currency.existability=2/(1+Math.exp(-game.currency.existivity))-1;
-  game.currency.time+=game.production.time*timeelapsed;
+  game.currency.etime+=game.production.etime*timeelapsed;
   game.currency.thought+=game.production.thought*timeelapsed;
   game.currency.energy+=game.production.energy*timeelapsed;
   if (game.unlocked.existivity){document.getElementById("disp.existivity").innerHTML="Your existivity is <span class=\"large\">"+Math.round(game.currency.existivity*100)/100+"</span> and has <span class=\"large\">"+Math.round(1000*game.currency.existability)/10+"%</span> change of existing.";}
   if (game.unlocked.existance){document.getElementById("disp.existance").innerHTML="You know <span class=\"large\">"+game.currency.existance+"</span> existances. They produce <span class=\"large\">"+game.production.existivity+"</span> existivity each second.";}
-  if (game.unlocked.time){document.getElementById("disp.time").innerHTML="Time: <span class=\"large\">"+game.currency.time+"</span>";}
+  if (game.unlocked.etime){document.getElementById("disp.etime").innerHTML="Existances experienced as much as <span class=\"large\">"+game.currency.etime+"</span> seconds. It boosts";}
   if (game.unlocked.thought){document.getElementById("disp.thought").innerHTML="Thoughts: <span class=\"large\">"+game.currency.thought+"</span>";}
   if (game.unlocked.energy){document.getElementById("disp.energy").innerHTML="Energy: <span class=\"large\">"+game.currency.energy+"</span>";}
 }
