378f62caf833b236d20f37fc1440973cf15c2ec2
Naruyoko <32781397+Naruyoko@users.noreply.github.com>
2018-03-15 21:12:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ var game={
   canbuy:{
     existance:(function (){return game.currency.existance<Math.pow(2,game.currency.etime+4);}),
     etime:(function (){return (game.currency.existance>=Math.pow(2,game.currency.etime+4))&&(game.currency.etime<game.currency.thought*2+4);}),
-    thought:(function (){return (game.currency.etime>=game.currency.thought*2+4)&&(game.currency.existivity>=50*Math.pow(game.currency.thought+2,2));}),
-    energy:(function (){return false;})
+    thought:(function (){return (game.currency.etime>=game.currency.thought*2+4)&&(game.currency.existivity>=50*Math.pow(game.currency.thought+2,2))&&!((game.currency.thought>=(game.currency.energy*(game.currency.energy+1)/2+3)&&(game.currency.word>=Math.floor(Math.pow(game.currency.energy,1.5))*25+100));}),
+    energy:(function (){return (game.currency.thought>=Math.floor(game.currency.energy*(game.currency.energy+1)/2+3))&&(game.currency.word>=Math.floor(Math.pow(game.currency.energy,1.5))*25+100);})
   }
 };
 function showhide(x,t){
@@ -45,8 +45,9 @@ var timeelapsed=0;
 var lasttime=0;
 var d=new Date();
 function updateprod(){
-  game.production.existivity=(Math.floor(game.currency.existance)+1)*0.01*Math.pow(1.2,game.currency.etime)
+  game.production.existivity=(Math.floor(game.currency.existance)+1)*0.01*Math.pow(1.2,game.currency.etime);
   game.production.existance=game.currency.word/100;
+  game.production.etime=Math.max(Math.pow(Math.floor(game.currency.existance,0.05))-1.4,0)+Math.max(Math.pow(game.currency.existivity,0.001)-1.0075,0);
 }
 function updatecurr(){
   if (lasttime!==0){
@@ -65,7 +66,7 @@ function updatedisp(){
   if (game.unlocked.etime){document.getElementById("disp.etime").innerHTML="Existances experienced as much as <span class=\"large\">"+Math.round(game.currency.etime)+"</span> seconds. It boosts the production of existivity by <span class=\"large\">"+Math.round(Math.pow(1.2,game.currency.etime)*100-100)+"%</span>.";}
   if (game.unlocked.thought){document.getElementById("disp.thought").innerHTML="You have <span class=\"large\">"+game.currency.thought+"</span> thoughts and think "+game.currency.thought+" strings per second.";}
   if (game.unlocked.thought){document.getElementById("disp.word").innerHTML="You have thought of <span class=\"large\">"+game.currency.word+"</span> <span title=\"More to do, Less you have to.\">words in the title</span> and finds <span class=\"large\">"+Math.round(game.production.existance*100)/100+"</span> existances per second. Recently generated: ";}
-  if (game.unlocked.energy){document.getElementById("disp.energy").innerHTML="Energy: <span class=\"large\">"+game.currency.energy+"</span>";}
+  if (game.unlocked.energy){document.getElementById("disp.energy").innerHTML="There are <span class=\"large\">"+game.currency.energy+"</span> joules of energy in your system. Existances and existivity together experience <span \"large\">"+Math.round(game.production.etime*1000)*1000+"</span> seconds per second.";}
   if (!(document.getElementById("button.convexisti").className=="hidden")){
       if (game.canbuy.existance()){
       document.getElementById("button.convexisti").className="";
@@ -87,8 +88,16 @@ function updatedisp(){
       document.getElementById("button.convetime").className="unavailable";
     }
   }
+  if (!(document.getElementById("button.convthought").className=="hidden")){
+    if (game.canbuy.energy()){
+      document.getElementById("button.convthought").className="";
+    }else{
+      document.getElementById("button.convthought").className="unavailable";
+    }
+  }
   document.getElementById("button.convexista").innerHTML="Experience.<br/>Cost: "+Math.round(Math.pow(2,game.currency.etime+4))+" existances";
   document.getElementById("button.convetime").innerHTML="Thought for the words.<br/>Cost: "+Math.round(game.currency.thought*2+4)+" experienced time,<br/>  "+Math.round(50*Math.pow(game.currency.thought+2,2))+" existivity";
+  document.getElementById("button.convthought").innerHTML="Be energetic!!<br.>Cost: "+Math.floor(game.currency.energy*(game.currency.energy+1)/2+3)+" thoughts,<br/>  "+Math.floor(Math.pow(game.currency.energy,1.5))*25+100)+" words found";
 }
 function convexisti(){
   if (!game.canbuy.existance()){return;}
@@ -118,6 +127,7 @@ function convetime(){
   game.unlocked.thought=true;
   updateprod();
   var genwordinterval=setInterval(genword,1000/game.currency.thought);
+  if (game.currency.thought>=3){showhide(document.getElementById("button.convthoght"),true);}
 }
 function genword(){
   var word="";
@@ -130,5 +140,16 @@ function genword(){
   }
   document.getElementById("disp.genword").innerHTML=word;
   return word
+}
+function convthought(){
+  if (!game.canbuy.energy()){return;}
+  game.currency.energy++;
+  game.currency.word=0;
+  game.currency.thought=0;
+  game.currency.etime=0;
+  game.currency.existivity=0;
+  game.currency.existance=0;
+  game.unlocked.energy=true;
+  updateprod();
 }
 loading=false;
